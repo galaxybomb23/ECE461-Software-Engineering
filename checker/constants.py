028cf9def628489327f54da8d4adbbb9d5184cf2
Elijah Jorgensen <jorgenel@purdue.edu>
2024-09-11 22:34:24 -0400

    added checker

---
+++ checker/constants.py
@@ -0,0 +1,42 @@
+import os
+
+## =================== UPDATE THESE VALUES =================== ##
+# Env variables
+GITHUB_TOKEN = os.getenv("API_TOKEN", "")
+LOG_FILE = os.getenv("LOG_FILE", "/tmp/checker.log")
+LOG_LEVEL = os.getenv("LOG_LEVEL", 2)
+
+# Repository location
+REPO_PATH = "/mnt/c/Users/Elija/Documents/GitHub/ECE461-Software-Engineering"
+
+
+
+## =================== DO NOT MODIFY BELOW THIS LINE =================== ##
+# constants
+ONE_URL = os.path.join(os.path.dirname(os.path.realpath(__file__)), "one-url.txt")
+
+# JSON Fields
+FIELDS = [
+    "URL",
+]
+
+SCORE_FIELDS = [
+    "NetScore",
+    "RampUp",
+    "Correctness",
+    "BusFactor",
+    "ResponsiveMaintainer",
+    "License"
+]
+
+LATENCY_FIELDS = [ f"{s}_Latency" for s in SCORE_FIELDS ]
+ALL_FIELDS = FIELDS + SCORE_FIELDS + LATENCY_FIELDS
+
+# printing Colors
+ESC="\033"
+RED=ESC+"[91m"
+GREEN=ESC+"[92m"
+YELLOW=ESC+"[93m"
+BLUE=ESC+"[94m"
+RESET=ESC+"[0m"
+BOLD=ESC+"[1m"
